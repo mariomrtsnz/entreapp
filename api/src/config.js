77022901fc93d047063d05e6477c72c486d806e8
juan antonio ortiz guerra <juanantonioortizguerra@gmail.com>
2019-01-09 18:01:15 +0100

    se aniade mlab al proyecto pero la ejecucion de la api no se efectua con exito

--- api/src/config.js
+++ api/src/config.js
@@ -39,7 +39,7 @@ const config = {
   test: { },
   development: {
     mongo: {
-      uri: 'mongodb://localhost/entreapp-dev',
+      uri: 'mongodb://entreappuser:salesianosDesarrollo2019@ds161700.mlab.com:61700/entreappdatabase',
       options: {
         debug: true
       }
@@ -49,7 +49,7 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/entreapp'
+      uri: process.env.MONGODB_URI || 'mongodb://entreappuser:salesianosDesarrollo2019@ds161700.mlab.com:61700/entreappdatabase'
     }
   }
 }
