4ff55f267f3c9a9ddf3a0a8db3e767c841fdf970
juan antonio <juanantonioortizguerra@gmail.com>
2019-01-10 08:59:23 +0100

    se diseña la tabla de usuarios

--- frontend-web/src/app/dashboard/user/user.component.ts
+++ frontend-web/src/app/dashboard/user/user.component.ts
@@ -4,15 +4,23 @@ import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
 
 import { MatPaginator, MatTableDataSource, MatSnackBar, MatDialog } from '@angular/material';
-
+export interface Persona {
+  name: string;
+  email: string;
+  points: number;
+}
+const ELEMENT_DATA: Persona[] = [
+  {name: 'pepe', email: 'pepe@pepe.com', points: 1.0079},
+  {name: 'miguel', email: 'miguel@miguel.com', points: 1.0079},
+];
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'email', 'points'];
-  dataSource;
+  displayedColumns: string[] = ['picture','name', 'email', 'points', 'actions'];
+  dataSource= ELEMENT_DATA;
   constructor(private snackBar: MatSnackBar, private router: Router, public dialog: MatDialog) { }
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
