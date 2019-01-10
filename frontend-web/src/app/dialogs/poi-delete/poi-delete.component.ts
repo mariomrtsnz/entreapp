fd4532345b840fc025e4d62ac15e83e6480387eb
Jesús Palma <lPalma.esp123@gmail.com>
2019-01-10 23:17:04 +0100

    fixed problem with poi-delete

--- frontend-web/src/app/dialogs/poi-delete/poi-delete.component.ts
+++ frontend-web/src/app/dialogs/poi-delete/poi-delete.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { PoiResponse } from 'src/app/interfaces/poi-response';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef, MatSnackBar } from '@angular/material';
+import { OnePoiResponse } from 'src/app/interfaces/one-poi-response';
 import { PoiService } from 'src/app/services/poi.service';
 
 @Component({
@@ -11,7 +11,6 @@ import { PoiService } from 'src/app/services/poi.service';
 })
 export class PoiDeleteComponent implements OnInit {
 
-  POI: PoiResponse;
   public form: FormGroup;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder,
@@ -19,7 +18,6 @@ export class PoiDeleteComponent implements OnInit {
   public snackBar: MatSnackBar) { }
 
   ngOnInit() {
-    this.POI = this.data.poi;
     this.createForm();
   }
 
@@ -30,7 +28,7 @@ export class PoiDeleteComponent implements OnInit {
   }
 
   onSubmit() {
-    this.poiService.remove(this.POI.id).toPromise()
+    this.poiService.remove(this.data.poi.id).toPromise()
     .then(resp => this.dialogRef.close(resp))
     .catch(() => this.snackBar.open('Error al borrar la localización.', 'Cerrar', {duration: 3000}));
   }
