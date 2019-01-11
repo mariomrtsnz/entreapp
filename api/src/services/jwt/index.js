4a1e6817353985a48d336ce25f8a802e57eea6be
Jesús Palma <lPalma.esp123@gmail.com>
2019-01-11 21:01:23 +0100

    JWT ExpirationDate added

--- api/src/services/jwt/index.js
+++ api/src/services/jwt/index.js
@@ -2,11 +2,20 @@ import jwt from 'jsonwebtoken'
 import Promise from 'bluebird'
 import { jwtSecret } from '../../config'
 
+const requireProcessEnv = (name) => {
+  if (!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable')
+  }
+  return process.env[name]
+}
+
 const jwtSign = Promise.promisify(jwt.sign)
 const jwtVerify = Promise.promisify(jwt.verify)
+const jwtOptions = { expiresIn: requireProcessEnv('TOKEN_LIFE') }
 
-export const sign = (id, options, method = jwtSign) =>
+export const sign = (id, options = jwtOptions, method = jwtSign) =>
   method({ id }, jwtSecret, options)
+  
 
 export const signSync = (id, options) => sign(id, options, jwt.sign)
 
