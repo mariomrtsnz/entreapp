a47b678fa643b333cbfae7484cb5050bb98c4036
juan antonio ortiz guerra <juanantonioortizguerra@gmail.com>
2019-01-15 21:09:31 +0100

    se crean usuarios con exito y se crea metodo autogenerar password

--- frontend-web/src/app/dto/create-user.dto.ts
+++ frontend-web/src/app/dto/create-user.dto.ts
@@ -6,4 +6,13 @@ export class UserCreateDto {
         picture: String;
         city: String;
         language: String;
+        constructor(e: string, p: string, n: string, role: string, pic: string, ci: string, langua: string) {
+                this.email = e;
+                this.password = p;
+                this.name = n;
+                this.role = role;
+                this.picture = pic;
+                this.city = ci;
+                this.language = langua;
+            }
 }
--- frontend-web/src/app/interfaces/user-response.ts
+++ frontend-web/src/app/interfaces/user-response.ts
@@ -5,4 +5,6 @@ export interface UserResponse {
     picture: string;
     email: string;
     createAt: string;
+    country: string;
+    languaje: string;
 }
