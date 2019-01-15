add33a946f32030630114abf18608ab8f661bdd6
juan antonio <juanantonioortizguerra@gmail.com>
2019-01-15 13:40:52 +0100

    se trbaja sobre crear user

--- frontend-web/src/app/dialogs/dialog-create-user/dialog-create-user.component.ts
+++ frontend-web/src/app/dialogs/dialog-create-user/dialog-create-user.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UserResponse } from '../../interfaces/user-response';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { MatDialogRef, MatSnackBar } from '@angular/material';
 import { UserCreateDto } from 'src/app/dto/create-user.dto';
 import { Roles } from 'src/app/interfaces/roles';
 import { CountryResponse } from 'src/app/interfaces/country-response';
+import { CustomValidators } from 'ng2-validation';
+
+const password = new FormControl('', Validators.required);
+
+const confirmPassword = new FormControl('', CustomValidators.equalTo(password));
 
 @Component({
   selector: 'app-dialog-create-user',
@@ -18,6 +23,11 @@ export class DialogCreateUserComponent implements OnInit {
   roleSelected;
   countrySelected;
   public form: FormGroup;
+  hide = true;
+  passGenerada=''
+  passGeneradaDos=''
+  hideDos = true;
+  type='password'
   roles: string[];
   constructor(private fb: FormBuilder, private userService: UserService,
     public dialogRef: MatDialogRef<DialogCreateUserComponent>, public snackBar: MatSnackBar) { }
@@ -28,50 +38,70 @@ export class DialogCreateUserComponent implements OnInit {
     this.getAllCountries();
     // this.getData();
   }
-/*email: String;
-        password: String;
-        name: String;
-        role: String;
-        picture: String;
-        city: String;
-        language: String;*/
-  createForm() {
-    this.form = this.fb.group ( {
-      email: [null, Validators.compose ([ Validators.required, Validators.email ])],
-      name: [null, Validators.compose ([ Validators.required ])],
-      role: [null, Validators.compose ([ Validators.required ])],
-      country: [null, Validators.compose ([ Validators.required ])],
-      languaje: [null, Validators.compose ([ Validators.required ])],
-      picture: [null, Validators.compose ([ Validators.required ])],
+  /*email: String;
+          password: String;
+          name: String;
+          role: String;
+          picture: String;
+          city: String;
+          language: String;*/
+  randomPassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+      var i = Math.floor(Math.random() * chars.length);
+      pass += chars.charAt(i);
+    }
+    return pass;
+  }
+  generate() {
+    let tamanioPassword = 10;
+    let passwordGenerada = this.randomPassword(tamanioPassword);
+    this.passGenerada = passwordGenerada;
+    this.passGeneradaDos = passwordGenerada;
+    this.type = "password";
 
-    } );
+
+  }
+  createForm() {
+    this.form = this.fb.group({
+      email: [null, Validators.compose([Validators.required, Validators.email])],
+      password: password,
+      confirmPassword: confirmPassword,
+      name: [null, Validators.compose([Validators.required])],
+      role: [null, Validators.compose([Validators.required])],
+      country: [null, Validators.compose([Validators.required])],
+      languaje: [null, Validators.compose([Validators.required])],
+      picture: [null, Validators.compose([Validators.required])],
+
+    });
   }
 
   getAllCountries() {
     this.userService.getAllCountries().toPromise()
-    .then(receivedCountries => {
-      this.countries = receivedCountries;
-    })
-    .catch(() => this.snackBar.open('There was an error when we were loading data.', 'Close', {duration: 3000}));
+      .then(receivedCountries => {
+        this.countries = receivedCountries;
+      })
+      .catch(() => this.snackBar.open('There was an error when we were loading data.', 'Close', { duration: 3000 }));
   }
   onSubmit() {
     const newUser: UserCreateDto = <UserCreateDto>this.form.value;
     newUser.picture = 'prueba';
     console.log(newUser);
     this.userService.create(newUser).toPromise()
-    .then(resp => {
-      console.log('aqui');
-      console.log(resp);
-      this.dialogRef.close(resp);
-    })
-    .catch(() => this.snackBar.open('Error creating user.', 'Cerrar', {duration: 3000}));
+      .then(resp => {
+        console.log('aqui');
+        console.log(resp);
+        this.dialogRef.close(resp);
+      })
+      .catch(() => this.snackBar.open('Error creating user.', 'Cerrar', { duration: 3000 }));
   }
   obtainRoles() {
     this.userService.getRoles().toPromise()
-    .then(receivedRoles => {
-      this.roles = receivedRoles.roles;
-    })
-    .catch(() => this.snackBar.open('There was an error when we were loading data.', 'Close', {duration: 3000}));
+      .then(receivedRoles => {
+        this.roles = receivedRoles.roles;
+      })
+      .catch(() => this.snackBar.open('There was an error when we were loading data.', 'Close', { duration: 3000 }));
   }
   /*onSubmit() {
     const newPoi: PoiCreateDto = <PoiCreateDto>this.form.value;
