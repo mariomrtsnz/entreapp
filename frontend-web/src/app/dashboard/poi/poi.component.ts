a8e79da71a65c7dfadf5c1235e7a8035b033cf1f
palma9 <lPalma.esp123@gmail.com>
2019-01-16 18:35:55 +0100

    cosas del poi

--- frontend-web/src/app/dashboard/poi/poi.component.ts
+++ frontend-web/src/app/dashboard/poi/poi.component.ts
@@ -4,11 +4,9 @@ import { Router } from '@angular/router';
 import { DialogPoiCreateComponent } from 'src/app/dialogs/dialog-poi-create/dialog-poi-create.component';
 import { DialogPoiDeleteComponent } from 'src/app/dialogs/dialog-poi-delete/dialog-poi-delete.component';
 import { DialogPoiEditComponent } from 'src/app/dialogs/dialog-poi-edit/dialog-poi-edit.component';
-import { AuthenticationService } from 'src/app/services/authentication.service';
 import { PoiService } from 'src/app/services/poi.service';
 
 import { PoiResponse } from './../../interfaces/poi-response';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-poi',
@@ -20,12 +18,11 @@ export class PoiComponent implements OnInit {
   POIs: PoiResponse;
 
 
-  constructor(private poiService: PoiService, public dialog: MatDialog, private userService: UserService,
-    private authService: AuthenticationService, public router: Router, public snackBar: MatSnackBar) { }
+  constructor(private poiService: PoiService, public dialog: MatDialog,
+    public router: Router, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.getAll();
-    this.userService.getMe().subscribe(u => console.log(u));
   }
 
   getAll() {
