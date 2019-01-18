25d6725de360d15769c872bbb173c0e3bad39a8b
Jesús Palma <lPalma.esp123@gmail.com>
2019-01-18 22:38:00 +0100

    POI Details buttons

--- frontend-web/src/app/dashboard/poi-details/poi-details.component.ts
+++ frontend-web/src/app/dashboard/poi-details/poi-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { OnePoiResponse } from 'src/app/interfaces/one-poi-response';
 import { PoiService } from 'src/app/services/poi.service';
 import { Router } from '@angular/router';
+import { DialogPoiDeleteComponent } from 'src/app/dialogs/dialog-poi-delete/dialog-poi-delete.component';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-poi-details',
@@ -13,9 +15,10 @@ export class PoiDetailsComponent implements OnInit {
   poi: OnePoiResponse;
   coverImage: string;
   images = ['https://bit.ly/2AHGQIw', 'https://bit.ly/2QCBEuO', 'https://on.natgeo.com/2TOioMO'];
+  showSettings = false;
 
-
-  constructor(private poiService: PoiService, public router: Router) { }
+  constructor(private poiService: PoiService, public router: Router,
+    public dialog: MatDialog, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
     if (this.poiService.selectedPoi == null) {
@@ -30,6 +33,17 @@ export class PoiDetailsComponent implements OnInit {
       this.poi = p;
       this.coverImage = p.coverImage;
     });
+  }
+
+  openEditPoi() {
+    this.poiService.selectedPoi = this.poi;
+    this.router.navigate(['home/edit']);
+  }
+
+  openDialogDeletePoi() {
+    const dialogDeletePoi = this.dialog.open(DialogPoiDeleteComponent, {data: {poi: this.poi}});
+    dialogDeletePoi.afterClosed().subscribe(res => res === 'confirm' ? this.router.navigate['/home'] : null,
+    err => this.snackBar.open('There was an error when we were deleting this POI.', 'Close', {duration: 3000}));
   }
 
 
