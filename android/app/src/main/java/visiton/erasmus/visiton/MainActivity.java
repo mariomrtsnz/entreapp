d9522350e5ea3496fd688e73886e3b6d3084b384
JoseAlbertoVazq <josealbertovz@gmail.com>
2019-02-05 12:19:50 +0100

    creada estructura para empezar retrofit

--- android/app/src/main/java/visiton/erasmus/visiton/MainActivity.java
+++ android/app/src/main/java/visiton/erasmus/visiton/MainActivity.java
@@ -3,6 +3,9 @@ package visiton.erasmus.visiton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import visiton.erasmus.visiton.model.Language;
+import visiton.erasmus.visiton.model.Poi;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
