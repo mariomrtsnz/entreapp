ec2bb7976ef5b0fd2aa15936b7934c606642e9bf
JoseAlbertoVazq <josealbertovz@gmail.com>
2019-02-05 13:19:02 +0100

    PoiService

---
+++ android/app/src/main/java/visiton/erasmus/visiton/retrofit/services/PoiService.java
@@ -0,0 +1,16 @@
+package visiton.erasmus.visiton.retrofit.services;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import visiton.erasmus.visiton.model.Poi;
+import visiton.erasmus.visiton.responses.ResponseContainer;
+
+public interface PoiService {
+
+    @GET("/pois")
+    Call<ResponseContainer<Poi>> listPois();
+
+    @GET("/poi/{id}")
+    Call<Poi> getPoi(@Path("id") Long id);
+}
