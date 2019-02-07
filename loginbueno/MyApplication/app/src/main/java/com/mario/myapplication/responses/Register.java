4c761860b9154591ce88afc8a26b149848e06712
JoseAlbertoVazq <danielguillen10>
2019-02-07 17:50:17 +0100

    responses traspasados, añadidas retrofit y gson

---
+++ loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/Register.java
@@ -0,0 +1,68 @@
+package com.mario.myapplication.responses;
+
+public class Register {
+
+    private String name;
+    private String email;
+    private String password;
+    private String picture;
+    private String role;
+
+    public Register() {
+    }
+
+    public Register(String name, String email, String password, String picture, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public Register(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = "user";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
