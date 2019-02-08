f30ff836ec9a80a3c435c3b018f3d84485c30bdd
JoseAlbertoVazq <josealbertovz@gmail.com>
2019-02-08 10:54:31 +0100

    POJOs audioguide description y translations necesarias creadas y listas

---
+++ loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/model/Audioguide.java
@@ -0,0 +1,40 @@
+package com.mario.myapplication.model;
+
+public class Audioguide {
+
+    private Language language;
+    private String originalFile;
+    private Translation[] translations;
+
+    public Audioguide() {}
+
+    public Audioguide(Language language, String originalFile, Translation[] translations) {
+        this.language = language;
+        this.originalFile = originalFile;
+        this.translations = translations;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public String getOriginalFile() {
+        return originalFile;
+    }
+
+    public void setOriginalFile(String originalFile) {
+        this.originalFile = originalFile;
+    }
+
+    public Translation[] getTranslations() {
+        return translations;
+    }
+
+    public void setTranslations(Translation[] translations) {
+        this.translations = translations;
+    }
+}
