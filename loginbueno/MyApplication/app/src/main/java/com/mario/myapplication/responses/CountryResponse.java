0053e5c50278ea3d3e98d9248677f740d9220bb2
JoseAlbertoVazq <josealbertovz@gmail.com>
2019-02-08 10:44:00 +0100

    CountryResponse

--- loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/CountryResponse.java
+++ loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/CountryResponse.java
@@ -1,4 +1,29 @@
 package com.mario.myapplication.responses;
 
 public class CountryResponse {
+
+    private String name;
+
+    public CountryResponse () {
+
+    }
+
+    public CountryResponse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryResponse{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
