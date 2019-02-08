24039044fe0d6d53a39bd0df4a70cfef0bfc6cb3
JoseAlbertoVazq <josealbertovz@gmail.com>
2019-02-08 10:39:29 +0100

    UserResponse, BadgeResponse

--- loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/BadgeResponse.java
+++ loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/BadgeResponse.java
@@ -1,4 +1,75 @@
 package com.mario.myapplication.responses;
 
+import java.util.Arrays;
+
 public class BadgeResponse {
+
+    private String name;
+    private float points;
+    private String description;
+    private String icon;
+    private PoiResponse[] pois;
+
+    public BadgeResponse() {
+
+    }
+
+    public BadgeResponse(String name, float points, String description, String icon, PoiResponse[] pois) {
+        this.name = name;
+        this.points = points;
+        this.description = description;
+        this.icon = icon;
+        this.pois = pois;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPoints() {
+        return points;
+    }
+
+    public void setPoints(float points) {
+        this.points = points;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public PoiResponse[] getPois() {
+        return pois;
+    }
+
+    public void setPois(PoiResponse[] pois) {
+        this.pois = pois;
+    }
+
+    @Override
+    public String toString() {
+        return "BadgeResponse{" +
+                "name='" + name + '\'' +
+                ", points=" + points +
+                ", description='" + description + '\'' +
+                ", icon='" + icon + '\'' +
+                ", pois=" + Arrays.toString(pois) +
+                '}';
+    }
 }
--- loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/UserResponse.java
+++ loginbueno/MyApplication/app/src/main/java/com/mario/myapplication/responses/UserResponse.java
@@ -1,4 +1,128 @@
 package com.mario.myapplication.responses;
 
 public class UserResponse {
+
+    private int id;
+    private String name;
+    private String role;
+    private String picture;
+    private String password;
+    private String email;
+    private String createAt;
+    private String country;
+    private String language;
+    private BadgeResponse badges;
+
+    public UserResponse() {
+
+    }
+
+    public UserResponse(int id, String name, String role, String picture, String password, String email, String createAt, String country, String language, BadgeResponse badges) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.picture = picture;
+        this.password = password;
+        this.email = email;
+        this.createAt = createAt;
+        this.country = country;
+        this.language = language;
+        this.badges = badges;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public BadgeResponse getBadges() {
+        return badges;
+    }
+
+    public void setBadges(BadgeResponse badges) {
+        this.badges = badges;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                ", picture='" + picture + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", createAt='" + createAt + '\'' +
+                ", country='" + country + '\'' +
+                ", language='" + language + '\'' +
+                ", badges=" + badges +
+                '}';
+    }
 }
