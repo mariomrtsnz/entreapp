78eeb06979c7d98095279364fffd4c263ded8306
JoseAlbertoVazq <josealbertovz@gmail.com>
2019-02-11 10:59:26 +0100

    Category Fragment y adapter(en proceso) 2

--- MyApplication/app/src/main/java/com/mario/myapplication/ui/categories/MyPoiRecyclerViewAdapter.java
+++ MyApplication/app/src/main/java/com/mario/myapplication/ui/categories/MyPoiRecyclerViewAdapter.java
@@ -1,28 +1,32 @@
-package com.mario.myapplication;
+package com.mario.myapplication.ui.categories;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.mario.myapplication.PoiFragment.OnListFragmentInteractionListener;
-import com.mario.myapplication.dummy.DummyContent.DummyItem;
+import com.mario.myapplication.R;
+import com.mario.myapplication.responses.CategoryResponse;
+import com.mario.myapplication.ui.categories.CategoryFragment.OnListFragmentInteractionListener;
+
 
 import java.util.List;
 
 /**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * {@link RecyclerView.Adapter} that can display a {@link} and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
  * TODO: Replace the implementation with code for your data type.
  */
-public class MyPoiRecyclerViewAdapter extends RecyclerView.Adapter<MyPoiRecyclerViewAdapter.ViewHolder> {
+class MyCategoryRecyclerViewAdapter extends RecyclerView.Adapter<MyCategoryRecyclerViewAdapter.ViewHolder> {
 
-    private final List<DummyItem> mValues;
+    private final List<CategoryResponse> mValues;
     private final OnListFragmentInteractionListener mListener;
 
-    public MyPoiRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+    public MyCategoryRecyclerViewAdapter(Context ctx, int layout, List<CategoryResponse> items, OnListFragmentInteractionListener listener) {
         mValues = items;
         mListener = listener;
     }
@@ -30,15 +34,20 @@ public class MyPoiRecyclerViewAdapter extends RecyclerView.Adapter<MyPoiRecycler
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_poi, parent, false);
+                .inflate(R.layout.fragment_category, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        holder.name.setText(mValues.get(position).getName());
+        holder.name.setText(mValues.get(position).getCategory().getName());
+        if(mValues.get(position).isFav()){
+            holder.fav.setImageResource(R.drawable.ic_fav_24dp);
+        }else{
+            holder.fav.setImageResource(R.drawable.ic_nofav_24dp);
+        }
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,20 +68,18 @@ public class MyPoiRecyclerViewAdapter extends RecyclerView.Adapter<MyPoiRecycler
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
-        public DummyItem mItem;
+        public final TextView name;
+        public final TextView parent;
+        public final ImageView fav;
+        public CategoryResponse mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
-            mContentView = (TextView) view.findViewById(R.id.content);
+            name = view.findViewById(R.id.category_name);
+            parent = view.findViewById(R.id.category_parent);
+            fav = view.findViewById(R.id.category_fav);
         }
 
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
-        }
     }
 }
