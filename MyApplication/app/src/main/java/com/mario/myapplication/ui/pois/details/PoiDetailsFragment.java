d9f307ac696871c3d0750b693c92faea09b3cf17
N3XUS123 <lPalma.esp123@gmail.com>
2019-02-15 14:24:33 +0100

    vamo al cibele Mario

--- MyApplication/app/src/main/java/com/mario/myapplication/ui/pois/details/PoiDetailsFragment.java
+++ MyApplication/app/src/main/java/com/mario/myapplication/ui/pois/details/PoiDetailsFragment.java
@@ -1,25 +1,42 @@
 package com.mario.myapplication.ui.pois.details;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.mario.myapplication.R;
+import com.mario.myapplication.responses.PoiResponse;
+import com.mario.myapplication.retrofit.generator.AuthType;
+import com.mario.myapplication.retrofit.generator.ServiceGenerator;
+import com.mario.myapplication.retrofit.services.PoiService;
+import com.mario.myapplication.util.UtilToken;
 
+import java.util.ArrayList;
 import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class PoiDetailsFragment extends Fragment {
 
     private final String id;
+    private PoiResponse poi;
+    private Toolbar toolbar;
 
     public PoiDetailsFragment(String poiId) {
         id = poiId;
@@ -28,15 +45,16 @@ public class PoiDetailsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getPoiDetails();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_poi_details, container, false);
 
-        Toolbar toolbar = v.findViewById(R.id.toolbar);
+        toolbar = v.findViewById(R.id.toolbar);
         ((AppCompatActivity) Objects.requireNonNull(getActivity())).setSupportActionBar(toolbar);
+        toolbar.setTitle("");
 
         FloatingActionButton fab = v.findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -44,4 +62,33 @@ public class PoiDetailsFragment extends Fragment {
 
         return v;
     }
+
+    private void getPoiDetails() {
+        String jwt = UtilToken.getToken(getContext());
+        PoiService service = ServiceGenerator.createService(PoiService.class, jwt, AuthType.JWT);
+        Call<PoiResponse> call = service.getPoi(id);
+
+        call.enqueue(new Callback<PoiResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<PoiResponse> call, @NonNull Response<PoiResponse> response) {
+                if (response.code() != 200) {
+                    Toast.makeText(getActivity(), "Request Error", Toast.LENGTH_SHORT).show();
+                } else {
+                    poi = response.body();
+                    setDataOnView();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<PoiResponse> call, @NonNull Throwable t) {
+                Log.e("Network Failure", t.getMessage());
+                Toast.makeText(getActivity(), "Network Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setDataOnView() {
+        toolbar.setTitle(poi.getName());
+    }
+
 }
