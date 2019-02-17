903c50e824092195638fa5bdd951422470f85821
Jesús Palma <lPalma.esp123@gmail.com>
2019-02-17 15:51:12 +0100

    Avanzando con POIDetails

--- MyApplication/app/src/main/java/com/mario/myapplication/ui/pois/details/PoiDetailsFragment.java
+++ MyApplication/app/src/main/java/com/mario/myapplication/ui/pois/details/PoiDetailsFragment.java
@@ -1,30 +1,37 @@
 package com.mario.myapplication.ui.pois.details;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.mario.myapplication.R;
+import com.mario.myapplication.model.Category;
 import com.mario.myapplication.responses.PoiResponse;
 import com.mario.myapplication.retrofit.generator.AuthType;
 import com.mario.myapplication.retrofit.generator.ServiceGenerator;
 import com.mario.myapplication.retrofit.services.PoiService;
 import com.mario.myapplication.util.UtilToken;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 import retrofit2.Call;
@@ -64,7 +71,7 @@ public class PoiDetailsFragment extends Fragment {
     }
 
     private void getPoiDetails() {
-        String jwt = UtilToken.getToken(getContext());
+        String jwt = UtilToken.getToken(Objects.requireNonNull(getContext()));
         PoiService service = ServiceGenerator.createService(PoiService.class, jwt, AuthType.JWT);
         Call<PoiResponse> call = service.getPoi(id);
 
@@ -88,7 +95,35 @@ public class PoiDetailsFragment extends Fragment {
     }
 
     private void setDataOnView() {
+        View v = getView();
         toolbar.setTitle(poi.getName());
+        ((TextView) Objects.requireNonNull(v).findViewById(R.id.tv_poi_description)).setText(Html.fromHtml(poi.getDescription().getOriginalDescription()));
+        Glide.with(this).load(poi.getCoverImage()).into(new CustomViewTarget<View, Drawable>(Objects.requireNonNull(getView())) {
+            @Override
+            protected void onResourceCleared(@Nullable Drawable placeholder) {
+
+            }
+
+            @Override
+            public void onLoadFailed(@Nullable Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                Objects.requireNonNull(v).findViewById(R.id.app_bar).setBackground(resource);
+            }
+        });
+
+        ChipGroup cg = Objects.requireNonNull(v).findViewById(R.id.cg_categories);
+        for (Category c: poi.getCategories()) {
+            Chip cc = new Chip(Objects.requireNonNull(getContext()));
+            cc.setChipDrawable(ChipDrawable.createFromResource(Objects.requireNonNull(getContext()), R.xml.chip_categories));
+            cc.setText(c.getName());
+
+            cg.addView(cc);
+
+        }
     }
 
 }
